582bc0ed05a79a7bd919104bcdd0bed72273e467
Luiz Fernando <141454919+LuizBAHIA30@users.noreply.github.com>
2023-08-11 17:41:30 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(600, 600);
+background("black");
+}
+
+function draw() {
+  fill("red");
+  stroke("white");
+ 
+  if(mouseIsPressed){
+     rect(mouseX,mouseY,10,10)}
+}
